0b3a11645281904d84be4d9a81ba8d5ae2233021
AzryalMods <azryal15@gmail.com>
2021-06-30 06:25:18 +0700

    Menambahkan Langsung UI dan Codingan
    Semua codingan, Finish

--- TubesKPL_E-Pen/TubesKPL_E-Pen/SignUp.Designer.cs
+++ TubesKPL_E-Pen/TubesKPL_E-Pen/SignUp.Designer.cs
@@ -29,12 +29,233 @@ namespace TubesKPL_E_Pen
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkShowPass = new System.Windows.Forms.CheckBox();
+            this.inputConfirmPass = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.inputPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.inputEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inputUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label8.Font = new System.Drawing.Font("Poppins Medium", 10F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label8.Location = new System.Drawing.Point(248, 634);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 25);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Click Here";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins Medium", 10F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.Gray;
+            this.label7.Location = new System.Drawing.Point(33, 634);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(222, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Already Have an Account?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkCyan;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Poppins Medium", 14F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(53, 573);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 44);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chkShowPass
+            // 
+            this.chkShowPass.AutoSize = true;
+            this.chkShowPass.Font = new System.Drawing.Font("Poppins Medium", 10F, System.Drawing.FontStyle.Bold);
+            this.chkShowPass.ForeColor = System.Drawing.Color.Gray;
+            this.chkShowPass.Location = new System.Drawing.Point(178, 510);
+            this.chkShowPass.Name = "chkShowPass";
+            this.chkShowPass.Size = new System.Drawing.Size(152, 29);
+            this.chkShowPass.TabIndex = 18;
+            this.chkShowPass.Text = "Show Password";
+            this.chkShowPass.UseVisualStyleBackColor = true;
+            this.chkShowPass.CheckedChanged += new System.EventHandler(this.chkShowPass_CheckedChanged);
+            // 
+            // inputConfirmPass
+            // 
+            this.inputConfirmPass.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.inputConfirmPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputConfirmPass.Font = new System.Drawing.Font("Poppins", 12F);
+            this.inputConfirmPass.Location = new System.Drawing.Point(53, 476);
+            this.inputConfirmPass.Multiline = true;
+            this.inputConfirmPass.Name = "inputConfirmPass";
+            this.inputConfirmPass.PasswordChar = '•';
+            this.inputConfirmPass.Size = new System.Drawing.Size(277, 28);
+            this.inputConfirmPass.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Gray;
+            this.label6.Location = new System.Drawing.Point(48, 445);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Confirm Password";
+            // 
+            // inputPass
+            // 
+            this.inputPass.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.inputPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputPass.Font = new System.Drawing.Font("Poppins", 12F);
+            this.inputPass.Location = new System.Drawing.Point(53, 401);
+            this.inputPass.Multiline = true;
+            this.inputPass.Name = "inputPass";
+            this.inputPass.PasswordChar = '•';
+            this.inputPass.Size = new System.Drawing.Size(277, 28);
+            this.inputPass.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Gray;
+            this.label5.Location = new System.Drawing.Point(48, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 28);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password";
+            // 
+            // inputEmail
+            // 
+            this.inputEmail.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.inputEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputEmail.Font = new System.Drawing.Font("Poppins", 12F);
+            this.inputEmail.Location = new System.Drawing.Point(53, 326);
+            this.inputEmail.Multiline = true;
+            this.inputEmail.Name = "inputEmail";
+            this.inputEmail.Size = new System.Drawing.Size(277, 28);
+            this.inputEmail.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(48, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 28);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Email";
+            // 
+            // inputUsername
+            // 
+            this.inputUsername.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.inputUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputUsername.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputUsername.Location = new System.Drawing.Point(53, 252);
+            this.inputUsername.Multiline = true;
+            this.inputUsername.Name = "inputUsername";
+            this.inputUsername.Size = new System.Drawing.Size(277, 28);
+            this.inputUsername.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(48, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 28);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Poppins SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label2.Location = new System.Drawing.Point(46, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 42);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Create New Account";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Poppins SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(46, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 42);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Hi, Im E-PEN";
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(375, 812);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkShowPass);
+            this.Controls.Add(this.inputConfirmPass);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.inputPass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inputEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inputUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SignUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SignUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkShowPass;
+        private System.Windows.Forms.TextBox inputConfirmPass;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox inputPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox inputEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inputUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
