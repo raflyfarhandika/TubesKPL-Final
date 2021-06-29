ce20687e3d57c7fc06daae078a0fa26dbabc59df
AzryalMods <azryal15@gmail.com>
2021-06-30 06:37:29 +0700

    Add Coddingan V1
    Menambahkan Code untuk menjalankan ui, seperti mengisi username kalo kosong ada pop up, email, password, dan confirm password agar berfungsi. dan juga tombol register agar saat user selesai daftar muncul notif lalu berfindah ke halaman login

--- TubesKPL_E-Pen/TubesKPL_E-Pen/Program.cs
+++ TubesKPL_E-Pen/TubesKPL_E-Pen/Program.cs
@@ -16,7 +16,7 @@ namespace TubesKPL_E_Pen
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new SignUp());
         }
     }
 }
