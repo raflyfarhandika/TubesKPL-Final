b6e157083b3cfafafdd49f7cfe0aa17e7b9026d7
raflyfarhandika <raflyfarhandika@gmail.com>
2021-07-02 20:59:33 +0700

    Commit Final

---
+++ TubesKPL_E-Pen/E-Pen_Unit_Testing/IsiBiodataTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace E_Pen_Unit_Testing
+{
+    [TestClass]
+    public class IsiBiodataTest
+    {
+        // Membuat index untuk menggambarkan inputnya
+        public enum Month { Januari, Februari, Maret, April, Mei, Juni, Juli, Agustus, September, Oktober, November, Desember };
+        // Membuat Array untuk menggambarkan outputnya
+        public static int GetDays(Month bulan)
+        {
+            int[] dayPerMonth = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            // Mengembalikan hasil array
+            return dayPerMonth[(int)bulan];
+        }
+
+
+        [TestMethod()]
+        public void comboBox2_SelectedIndexChangedTest()
+        {
+            // Membuat Variabel baru untuk tes method
+            GetDays Test = new GetDays(31);
+        }
+    }
+
+    internal class GetDays
+    {
+        private object januari;
+
+        public GetDays(object januari)
+        {
+            this.januari = januari;
+        }
+    }
+}
---
+++ TubesKPL_E-Pen/E-Pen_Unit_Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("E-Pen_Unit_Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E-Pen_Unit_Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f87b844a-8e44-4318-9a88-88b4f6bf8d47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
