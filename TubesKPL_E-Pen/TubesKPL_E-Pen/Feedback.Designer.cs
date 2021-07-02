416222550eb542e57ec82aebfc03716999006fdc
Gregkids <giannugrahaindra@gmail.com>
2021-07-02 16:56:52 +0700

    Update Feedback

--- TubesKPL_E-Pen/TubesKPL_E-Pen/Feedback.Designer.cs
+++ TubesKPL_E-Pen/TubesKPL_E-Pen/Feedback.Designer.cs
@@ -3,21 +3,7 @@ namespace TubesKPL_E_Pen
 {
     partial class Feedback
     {
-        public enum Star { STAR1, STAR2, STAR3, STAR4, STAR5 };
-
-        public string getStarText(Star input)
-        {
-            string[] outputStar =
-            {
-                "1 Star",
-                "2 Star",
-                "3 Star",
-                "4 Star",
-                "5 Star"
-            };
-
-            return outputStar[(int)input];
-        }
+        FeedbackModel starText = new FeedbackModel();
 
         /// <summary>
         /// Required designer variable.
@@ -70,7 +56,7 @@ namespace TubesKPL_E_Pen
             this.button2Star.Name = "button2Star";
             this.button2Star.Size = new System.Drawing.Size(68, 75);
             this.button2Star.TabIndex = 1;
-            this.button2Star.Text = getStarText(Star.STAR2);
+            this.button2Star.Text = starText.getStarText(FeedbackModel.Star.STAR2);
             this.button2Star.UseVisualStyleBackColor = true;
             this.button2Star.Click += new System.EventHandler(this.button2Star_Click);
             // 
@@ -80,7 +66,7 @@ namespace TubesKPL_E_Pen
             this.button3Star.Name = "button3Star";
             this.button3Star.Size = new System.Drawing.Size(68, 75);
             this.button3Star.TabIndex = 2;
-            this.button3Star.Text = getStarText(Star.STAR3);
+            this.button3Star.Text = starText.getStarText(FeedbackModel.Star.STAR3);
             this.button3Star.UseVisualStyleBackColor = true;
             this.button3Star.Click += new System.EventHandler(this.button3Star_Click);
             // 
@@ -90,7 +76,7 @@ namespace TubesKPL_E_Pen
             this.button4Star.Name = "button4Star";
             this.button4Star.Size = new System.Drawing.Size(68, 75);
             this.button4Star.TabIndex = 3;
-            this.button4Star.Text = getStarText(Star.STAR4);
+            this.button4Star.Text = starText.getStarText(FeedbackModel.Star.STAR4);
             this.button4Star.UseVisualStyleBackColor = true;
             this.button4Star.Click += new System.EventHandler(this.button4Star_Click);
             // 
@@ -100,7 +86,7 @@ namespace TubesKPL_E_Pen
             this.button5Star.Name = "button5Star";
             this.button5Star.Size = new System.Drawing.Size(68, 75);
             this.button5Star.TabIndex = 4;
-            this.button5Star.Text = getStarText(Star.STAR5);
+            this.button5Star.Text = starText.getStarText(FeedbackModel.Star.STAR5);
             this.button5Star.UseVisualStyleBackColor = true;
             this.button5Star.Click += new System.EventHandler(this.button5Star_Click);
             // 
@@ -168,7 +154,7 @@ namespace TubesKPL_E_Pen
             this.button1Star.Name = "button1Star";
             this.button1Star.Size = new System.Drawing.Size(68, 75);
             this.button1Star.TabIndex = 12;
-            this.button1Star.Text = getStarText(Star.STAR1);
+            this.button1Star.Text = starText.getStarText(FeedbackModel.Star.STAR1);
             this.button1Star.UseVisualStyleBackColor = true;
             this.button1Star.Click += new System.EventHandler(this.button1Star_Click);
             // 
--- TubesKPL_E-Pen/TubesKPL_E-Pen/FeedbackModel.cs
+++ TubesKPL_E-Pen/TubesKPL_E-Pen/FeedbackModel.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace TubesKPL_E_Pen
 {
-    class FeedbackModel
+    public class FeedbackModel
     {
+        public FeedbackModel()
+        {
+
+        }
+
+        public enum Star { STAR1, STAR2, STAR3, STAR4, STAR5 };
+
+        public string getStarText(Star input)
+        {
+            // Table-driven untuk tulisan "Star".
+
+            string[] outputStar =
+            {
+                "1 Star",
+                "2 Star",
+                "3 Star",
+                "4 Star",
+                "5 Star"
+            };
+
+            return outputStar[(int)input];
+        }
+
         public void confirmButtonFeedback(string input)
         {
+            // Mengisi array dengan text dari TextField.
+
             string[] terms = new string[400];
             for (int runs = 0; runs < 400; runs++)
             {
